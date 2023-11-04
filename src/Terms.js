3a8321b73176a42ce9cfd2d5b450202778d97bfe
codetestingforcdinc <89401300+codetestingforcdinc@users.noreply.github.com>
2023-11-04 11:20:46 +0530

    Update Terms.js

--- src/Terms.js
+++ src/Terms.js
@@ -1,9 +1,59 @@
 import React from 'react';
-
+import Header from './Header';
+import Footer from './Footer';
+import './stylefortcnp.css'
 function Terms() {
   return (
     <div>
-      <p>A</p>
+      <p>Terms and Conditions for PTJS Inc. <br />
+
+
+
+These Terms and Conditions ("Terms") govern your use of the PTJS Inc. website and services. By accessing or using our website, you agree to comply with and be bound by these Terms. If you do not agree with these Terms, please do not use our services.
+
+1. User Registration
+
+1.1. To use certain features of our website, you may be required to register an account with PTJS Inc. You must provide accurate and complete information when registering.
+
+1.2. You are responsible for maintaining the security of your account credentials and any activities that occur under your account. If you suspect unauthorized access to your account, please notify us immediately.
+
+2. Use of Our Services
+
+2.1. You may use our services for lawful purposes only. You agree not to use our website for any unlawful or prohibited activity.
+
+2.2. You are responsible for any content you post on our platform, including job listings, resumes, reviews, comments, or other user-generated content. Content must adhere to our guidelines and must not infringe on the rights of others.
+
+3. Intellectual Property
+
+3.1. All content on PTJS Inc., including but not limited to text, graphics, logos, images, software, and design elements, is protected by intellectual property laws and is the property of PTJS Inc. You may not use or reproduce this content without our express written consent.
+
+4. Privacy and Data Collection
+
+4.1. Your use of our services is also governed by our Privacy Policy, which outlines our data collection and privacy practices. By using our services, you consent to our data collection and usage practices as described in the Privacy Policy.
+
+5. Termination
+
+5.1. We reserve the right to terminate or suspend your account and access to our services at our discretion, with or without cause and without prior notice.
+
+6. Disclaimers and Limitation of Liability
+
+6.1. Our services are provided on an "as-is" and "as available" basis. We make no warranties or representations regarding the accuracy, completeness, or reliability of the information or content on our website.
+
+6.2. In no event shall PTJS Inc. be liable for any direct, indirect, incidental, consequential, or punitive damages arising from your use of our services.
+
+7. Indemnification
+
+7.1. You agree to indemnify and hold PTJS Inc., its affiliates, and their respective officers, directors, employees, and agents, harmless from any claims, liabilities, damages, costs, or expenses resulting from your use of our services or violation of these Terms.
+
+8. Changes to these Terms
+
+8.1. We may update these Terms from time to time to reflect changes in our services or for legal and regulatory reasons. We will notify you of any significant changes through our website or other means.
+
+9. Contact Us
+
+If you have any questions or concerns about these Terms and Conditions, please contact us at exploretutorialsyt@gmail.com.
+
+</p>
     </div>
   );
 }
