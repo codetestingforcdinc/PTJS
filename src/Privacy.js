d695ba415d80024e325643b827c1742504ef1cbe
codetestingforcdinc <89401300+codetestingforcdinc@users.noreply.github.com>
2023-11-04 11:15:33 +0530

    Update Privacy.js

--- src/Privacy.js
+++ src/Privacy.js
@@ -1,9 +1,75 @@
 import React from 'react';
-
+import Header from './Header'
+import Footer from './Footer'
+import './stylefortcnp.css'
 function Privacy() {
   return (
     <div>
-      <p>A</p>
+     <Header/> 
+      <p>
+    
+
+Privacy Policy for PTJS Inc.
+
+
+
+1. Introduction
+
+Welcome to PTJS Inc., a part-time job finding website dedicated to helping you find the perfect part-time employment opportunities. At PTJS Inc., we take your privacy seriously and are committed to protecting your personal information. This Privacy Policy outlines how we collect, use, disclose, and safeguard your information when you use our website or related services.
+
+2. Information We Collect
+
+2.1. Personal Information: We may collect personal information when you create an account, such as your name, email address, phone number, and postal address.
+
+2.2. User-Generated Content: We collect information you provide when you post job listings, resumes, reviews, comments, or other content on our platform.
+
+2.3. Usage Data: We collect information about your use of our website, including your IP address, browser type, pages viewed, and the time and date of your visit.
+
+2.4. Cookies and Tracking Technologies: We use cookies and similar technologies to enhance your experience on our website. You can manage your cookie preferences through your browser settings.
+
+3. How We Use Your Information
+
+We may use your information for the following purposes:
+
+3.1. To provide, personalize, and improve our services.
+
+3.2. To communicate with you regarding your account, updates, and relevant job opportunities.
+
+3.3. To monitor and analyze user trends, preferences, and interactions on our platform.
+
+3.4. To prevent, detect, and address fraud, security breaches, and abuse of our services.
+
+4. Sharing Your Information
+
+We may share your information with the following:
+
+4.1. Employers and Job Seekers: Your job listings and resumes are visible to other registered users of PTJS Inc.
+
+4.2. Service Providers: We may share information with third-party service providers who assist us in delivering our services.
+
+4.3. Legal Compliance: We may share your information when required to do so by law, or to protect our rights, safety, or those of others.
+
+5. Your Choices
+
+You have the following choices regarding your personal information:
+
+5.1. Account Settings: You can update your account information and privacy settings at any time.
+
+5.2. Marketing Communications: You can opt out of receiving promotional emails by following the instructions provided in those emails.
+
+6. Security
+
+We take reasonable measures to protect your information from unauthorized access and disclosure. However, no online platform can guarantee absolute security.
+
+7. Changes to this Privacy Policy
+
+We may update this Privacy Policy to reflect changes in our practices or for legal and regulatory reasons. We will notify you of any significant changes through our website or other means.
+
+8. Contact Us
+
+If you have any questions or concerns about this Privacy Policy or our data practices, please contact us at exploretutorialsyt@gmail.com.
+    </p>
+  <Footer/>
     </div>
   );
 }
